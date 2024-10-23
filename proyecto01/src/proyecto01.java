7fb12bbda9a1d43235b02740e1d6a03c7596fda4
PatitoCorrea <rafaelcorrealopez21@gmail.com>
2024-10-23 12:11:46 -0500

    primer commit

---
+++ proyecto01/src/proyecto01.java
@@ -0,0 +1,5 @@
+public class proyecto01 {
+   public static void main(String[] args) {
+       System.out.println("Ejemplo de Java Manejado desde GitHub Versionado a traves de Git Creado por PatitoCorrea");
+    }    
+}
