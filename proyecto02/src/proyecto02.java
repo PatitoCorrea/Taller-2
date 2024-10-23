6e9ed200b85c75b011c6ca54629378aa027aa241
PatitoCorrea <rafaelcorrealopez21@gmail.com>
2024-10-23 12:15:04 -0500

    tercer commit, trabajando con Git y GitHubClassroom

---
+++ proyecto02/src/proyecto02.java
@@ -0,0 +1,5 @@
+public class proyecto02 {
+   public static void main(String args[]) {
+       System.out.println("Ejemplo dos basico de Java Versionado a traves de Git Manejado desde GitHub Ademas se usar GitHubClassroom");
+    }    
+}
